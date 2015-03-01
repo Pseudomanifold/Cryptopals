725d6ed7f4a3c20bbca1f1e44aa1516f0fbbe213
Bastian Rieck <bastian@rieck.ru>
2015-03-01 18:18:48 +0100

    Included expected output for base64 conversion

--- test_conversions.cc
+++ test_conversions.cc
@@ -1,7 +1,5 @@
 #include "conversions.hh"
 
-#include <iostream>
-
 int main(int, char**)
 {
   std::string s  = "Man is distinguished, not only by his reason, but by this singular passion "
@@ -9,7 +7,14 @@ int main(int, char**)
                    "delight in the continued and indefatigable generation of knowledge, exceeds "
                    "the short vehemence of any carnal pleasure.";
 
-  std::cout << toBase64(s.c_str(), s.length() ) << "\n";
+  std::string t = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz"
+                  "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg"
+                  "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu"
+                  "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo"
+                  "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+
+  if( toBase64( s.c_str(), s.length() ) != t )
+    return -1;
 
   return 0;
 }
