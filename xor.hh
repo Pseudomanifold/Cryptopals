6797baeb69de93d95b226b68a3f18387545cbde5
Bastian Rieck <bastian@rieck.ru>
2015-03-11 12:21:58 +0100

    Cleaned up old source files

--- xor.hh
+++
@@ -1,9 +0,0 @@
-std::string xorBuffers(const char* s, const char* t, unsigned int length)
-{
-  std::string result;
-
-  for(unsigned int i = 0; i < length; i++)
-    result.push_back( s[i] xor t[i] );
-
-  return result;
-}
