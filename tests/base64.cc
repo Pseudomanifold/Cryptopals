3a25088a93e4e5040dad4e8a6328269411a2c8d2
Bastian Rieck <bastian@rieck.ru>
2015-03-11 10:19:46 +0100

    Started preparing test for Base64 conversion

---
+++ tests/base64.cc
@@ -0,0 +1,19 @@
+#include <conversions/base64.hh>
+
+#include <string>
+
+int main(int, char**)
+{
+  std::string in  = "Man is distinguished, not only by his reason, but by this singular passion "
+                    "from other animals, which is a lust of the mind, that by a perseverance of "
+                    "delight in the continued and indefatigable generation of knowledge, exceeds "
+                    "the short vehemence of any carnal pleasure.";
+
+  std::string out = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz"
+                    "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg"
+                    "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu"
+                    "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo"
+                    "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+
+  return 0;
+}
