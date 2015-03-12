a81e0e73f7200829163430b97aab10e715d51189
Bastian Rieck <bastian@rieck.ru>
2015-03-12 14:42:13 +0100

    Updated tests

--- tests/decrypt_single_byte_xor.cc
+++ tests/decrypt_single_byte_xor.cc
@@ -5,7 +5,7 @@
 int main(int, char**)
 {
   std::string in  = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-  std::string out = crypto::decryptSingleByteXOR(in).second;
+  std::string out = std::get<2>( crypto::decryptSingleByteXOR(in) );
 
   if( out != "Cooking MC's like a pound of bacon" )
     return -1;
